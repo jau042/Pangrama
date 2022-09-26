0e42652398d3d9ccb084fe9595708efd019627c1
jau042 <79652522+jau042@users.noreply.github.com>
2022-09-26 14:38:26 -0500

    Add files via upload

---
+++ Pangrama/src/pangrama/Pangrama.java
@@ -0,0 +1,22 @@
+package pangrama;
+
+import java.util.*;
+
+public class Pangrama {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int a = leer.nextInt();
+        leer.nextLine();
+        for (int i = 0; i < a; i++) {
+            boolean alfabeto[] = new boolean[26];
+            
+             String linea = leer.nextLine();
+             int letrasDiferentes = 0;
+             
+             
+             
+        }
+    }
+    
+}
